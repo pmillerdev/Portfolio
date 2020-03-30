d23f59682d42c74bab026ee210cbacfd62bb00b7
pmillerdev <pmillerdev@gmail.com>
2020-03-30 16:07:13 +0100

    feat: create new portfolio website using contentful

--- manifest-config.js
+++ manifest-config.js
@@ -1,8 +1,8 @@
 const colors = require('./colors');
 
 module.exports = {
-  name: 'Mate Gatsby Starter',
-  short_name: 'Mate Gatsby Starter',
+  name: 'Peter Miller JavaScript Engineer',
+  short_name: 'Peter Miller JavaScript Engineer',
   start_url: '/',
   background_color: colors.background,
   theme_color: colors.primary,
